1297056f89eb5be23b9b94c783d365f52b29dccc
jackthedane <fader4ever@hotmail.com>
2018-12-10 20:38:48 +0100

    Setup basic search routes

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,17 +2,98 @@
 
 namespace App\Http\Controllers;
 
-class SearchController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+use App\Http\Controllers\Controller;
 
-    //
+class SearchController extends Controller {
+
+
+// ---- Query methods ---- //
+
+  /**
+   * Get the results, based on the query string passed
+   *
+   * @param  string  $query
+   * @return string
+   */
+  public function getByQuery($query) {
+    return $query;
+  }
+
+  // ---- Genre methods ---- //
+
+  /**
+   * Get results, based on genre
+   *
+   * @param  string  $genre
+   * 
+   * @return string
+   */
+  public function getByGenre($genre) {
+    return "Genre: $genre";
+  }
+
+  // ---- Year methods ---- //
+
+  /**
+   * Get results, based on year
+   *
+   * @param  int  $year 
+   * 
+   * @return string
+   */
+  public function getByYear($year) {
+    return "Year: $year";
+  }
+
+  // ---- Combined query, year and genre methods ---- //
+
+  /**
+   * Get results, based on year and genre
+   *
+   * @param  string  $genre
+   * @param  int  $year 
+   * 
+   * @return string
+   */
+  public function getByGenreYear($genre, $year) {
+    return "Genre: $genre. Year: $year";
+  }
+
+   /**
+   * Get results, based on query and year
+   *
+   * @param  string  $query
+   * @param  int  $year
+   * 
+   * @return string
+   */
+  public function getByQueryYear($query, $year) {
+    return "Query: $query. Year: $year";
+  }
+
+  /**
+   * Get results, based on query and genre
+   *
+   * @param  string  $query
+   * @param  string  $genre
+   * 
+   * @return string
+   */
+  public function getByQueryGenre($query, $genre) {
+    return "Query: $query. Genre: $genre";
+  }
+
+  /**
+   * Get results, based on query, genre and year
+   *
+   * @param  string  $query
+   * @param  string  $genre
+   * @param  int  $year
+   * 
+   * @return string
+   */
+  public function getByQueryGenreYear($query, $genre, $year) {
+    return "Query: $query. Genre: $genre. Year: $year";
+  }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,60 @@
 //     return $router->app->version();
 // });
 
+// -- Search routes
 $router->group(['prefix' => 'search'], function () use ($router) {
-  
 
-  $router->get('/{query}', function ($query = null) {
-    if (!empty($query)) {
-      return $query;
-    }
+  // Endpoints needed
+  // Search query
+  // query/year
+  // query/genre
+  // query/genre/year
+
+  $router->get('/', function () {
+    return;
   });
+
+  // Query only
+  $router->get('/{query}', 'SearchController@getByQuery');
+
+  // Query and year
+  $router->get('/{query}/year/{year}', 'SearchController@getByQueryYear');
+
+  // Query and genre
+  $router->get('/{query}/genre/{genre}', 'SearchController@getByQueryGenre');
+
+  // All
+  $router->get('/{query}/genre/{genre}/year/{year}', 'SearchController@getByQueryGenreYear');
+  $router->get('/{query}/year/{year}/genre/{genre}', 'SearchController@getByQueryGenreYear');
+
+});
+
+// -- Year routes
+$router->group(['prefix' => 'year'], function () use ($router) {
+
+  $router->get('/', function () {
+    return;
+  });
+
+  // Year only
+  $router->get('/{year}', 'SearchController@getByYear');
+
+  // Year and genre
+  $router->get('/{year}/genre/{genre}', 'SearchController@getByGenreYear');
+
+});
+
+// -- Genre routes
+$router->group(['prefix' => 'genre'], function () use ($router) {
+
+  $router->get('/', function () {
+    return;
+  });
+
+  // Genre only
+  $router->get('/{genre}', 'SearchController@getByGenre');
+
+  // Genre and year
+  $router->get('/{genre}/year/{year}', 'SearchController@getByGenreYear');
+
 });
